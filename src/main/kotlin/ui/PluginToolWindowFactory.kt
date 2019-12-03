b1f786a951f07374b029cb171a3751c3e727f200
elena-lyulina <elena.lyulina@gmail.com>
2019-12-03 14:23:25 +0300

    Make plagin panel be in the center of the toolwindow

--- src/main/kotlin/ui/PluginToolWindowFactory.kt
+++ src/main/kotlin/ui/PluginToolWindowFactory.kt
@@ -3,14 +3,24 @@ package ui
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ToolWindowManager
 import javafx.application.Platform
 import javafx.embed.swing.JFXPanel
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.paint.Color
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.GridBagConstraints
+import java.awt.GridLayout
 import java.util.logging.Logger
-import javax.swing.JComponent
+import javax.swing.*
+import javax.swing.Box.createVerticalGlue
+import javax.swing.Box.createHorizontalGlue
+import javax.swing.text.StyleConstants.getComponent
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.openapi.wm.impl.ToolWindowImpl
 
 
 class PluginToolWindowFactory : ToolWindowFactory {
@@ -23,10 +33,13 @@ class PluginToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         log.info("create tool window")
         val content = createContent()
-        toolWindow.component.add(content)
+        toolWindow.component.parent.add(content)
+        toolWindow as ToolWindowImpl
     }
 
     private fun createContent() : JComponent {
+        val panel = JPanel()
+
         val fxPanel = JFXPanel()
         val controller = Controller()
         Platform.setImplicitExit(false);
@@ -34,10 +47,14 @@ class PluginToolWindowFactory : ToolWindowFactory {
             val loader = FXMLLoader(javaClass.classLoader.getResource("ui-form.fxml"))
             loader.setController(controller)
             val root = loader.load<Parent>()
-            val scene = Scene(root, Color.ALICEBLUE)
+            val scene = Scene(root, Color.WHITE)
             fxPanel.scene = scene
+            fxPanel.background = java.awt.Color.white
         }
-        return fxPanel
+
+        panel.background = java.awt.Color.white
+        panel.add(fxPanel)
+        return panel
     }
 }
 
