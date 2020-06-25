e4c45ec6997ef0b7d430b8a5e94e0828409ecea7
nbirillo <nbirillo@mail.ru>
2020-06-25 18:32:24 +0300

    Delete queries tests

--- src/main/kotlin/org/jetbrains/research/ml/codetracker/InitActivity.kt
+++ src/main/kotlin/org/jetbrains/research/ml/codetracker/InitActivity.kt
@@ -1,25 +1,19 @@
 package org.jetbrains.research.ml.codetracker
 
-import org.jetbrains.research.ml.codetracker.*
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.util.Disposer
-import org.jetbrains.research.ml.codetracker.server.PluginServer
 import org.jetbrains.research.ml.codetracker.ui.ServerDialogWrapper
 
 
 class InitActivity : StartupActivity {
     private val logger: Logger = Logger.getInstance(javaClass)
 
-    val pluginServer: PluginServer = PluginServer
-
     init {
         logger.info("${Plugin.PLUGIN_ID}: startup activity")
-        println(pluginServer.getCountries())
-
         Disposer.register(
             ApplicationManager.getApplication(),
             Disposable {
