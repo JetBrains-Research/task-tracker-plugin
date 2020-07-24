9a40a195ae7c3c905a424bf50a23ae29edcbd18f
nbirillo <nbirillo@mail.ru>
2020-07-24 16:07:00 +0300

    Add the task solving error text from server

--- src/main/kotlin/org/jetbrains/research/ml/codetracker/tracking/DocumentLogger.kt
+++ src/main/kotlin/org/jetbrains/research/ml/codetracker/tracking/DocumentLogger.kt
@@ -7,23 +7,12 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.io.FileUtil
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVPrinter
-<<<<<<< HEAD:src/main/kotlin/org/jetbrains/research/ml/codetracker/tracking/DocumentLogger.kt
 import org.jetbrains.research.ml.codetracker.Plugin
-=======
-import org.jetbrains.research.ml.codetracker.data.DocumentChangeData
->>>>>>> dev:src/main/kotlin/org/jetbrains/research/ml/codetracker/DocumentLogger.kt
 import org.jetbrains.research.ml.codetracker.server.TrackerQueryExecutor
-import org.joda.time.DateTime
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
 import java.nio.charset.StandardCharsets
-
-
-
-
-
-
 
 object DocumentLogger {
     data class Printer(val csvPrinter: CSVPrinter, val fileWriter: OutputStreamWriter, val file: File)
--- src/main/kotlin/org/jetbrains/research/ml/codetracker/tracking/ReadOnlyDialogWrapper.kt
+++ src/main/kotlin/org/jetbrains/research/ml/codetracker/tracking/ReadOnlyDialogWrapper.kt
@@ -3,10 +3,9 @@ package org.jetbrains.research.ml.codetracker.tracking
 
 import com.intellij.openapi.ui.DialogWrapper
 import org.jetbrains.research.ml.codetracker.models.Task
-import org.jetbrains.research.ml.codetracker.models.TaskChoosePaneText
+import org.jetbrains.research.ml.codetracker.models.TaskSolvingErrorText
 import org.jetbrains.research.ml.codetracker.server.PluginServer
 import org.jetbrains.research.ml.codetracker.ui.panes.SurveyUiData
-import org.jetbrains.research.ml.codetracker.ui.panes.util.LanguagePaneUiData
 import org.jetbrains.research.ml.codetracker.ui.panes.util.LowerCaseFormatter
 import java.awt.BorderLayout
 import java.awt.Dimension
@@ -17,6 +16,10 @@ import javax.swing.JPanel
 
 class ReadOnlyDialogWrapper(private val task: Task) : DialogWrapper(true) {
     private val formatter = LowerCaseFormatter()
+    private val defaultTaskSolvingErrorText = TaskSolvingErrorText(
+        "Task solving",
+        "You cannot edit this file until you choose the task. To start or continue solving the %s task, choose it on the task choosing panel in the codetracker plugin and press %s."
+    )
 
     override fun createCenterPanel(): JComponent? {
         val dialogPanel = JPanel(BorderLayout())
@@ -26,15 +29,19 @@ class ReadOnlyDialogWrapper(private val task: Task) : DialogWrapper(true) {
     }
 
     private fun createMessage(): String {
-//        todo: get from server
         val currentLanguage = SurveyUiData.language.currentValue
-        return "<html>Для начала решения задачи <b>${task.infoTranslation[currentLanguage]?.name}</b>" +
-                "выберите эту задачу на панели codetracker и нажмите " +
-                "<b>${PluginServer.paneText?.taskChoosePane?.get(currentLanguage)?.startSolving?.let { formatter.format(it)} }</b>"
+        val dialogText = PluginServer.taskSolvingErrorDialogText?.translation?.get(currentLanguage)?.description
+            ?: defaultTaskSolvingErrorText.description
+        return "<html>${java.lang.String.format(
+            dialogText,
+            task.infoTranslation[currentLanguage]?.name,
+            PluginServer.paneText?.taskChoosingPane?.get(currentLanguage)?.startSolving?.let { formatter.format(it) })}</html>"
     }
 
     init {
         init()
-        title = "Задача не выбрана"
+        val currentLanguage = SurveyUiData.language.currentValue
+        title = PluginServer.taskSolvingErrorDialogText?.translation?.get(currentLanguage)?.header
+            ?: defaultTaskSolvingErrorText.header
     }
 }
