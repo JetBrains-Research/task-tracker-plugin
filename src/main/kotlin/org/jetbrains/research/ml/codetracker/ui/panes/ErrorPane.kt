746db188c79714971f1c399cdc808a73eec1d8c3
nbirillo <nbirillo@mail.ru>
2020-07-27 23:24:33 +0300

    Fix scalable for the error pane

--- src/main/kotlin/org/jetbrains/research/ml/codetracker/ui/panes/ErrorPane.kt
+++ src/main/kotlin/org/jetbrains/research/ml/codetracker/ui/panes/ErrorPane.kt
@@ -8,6 +8,7 @@ import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.Pane
+import javafx.scene.shape.Polygon
 import org.jetbrains.research.ml.codetracker.server.PluginServer
 import org.jetbrains.research.ml.codetracker.ui.panes.util.PaneController
 import org.jetbrains.research.ml.codetracker.ui.panes.util.PaneControllerManager
@@ -30,11 +31,16 @@ class ErrorController(project: Project, scale: Double, fxPanel: JFXPanel, id: In
     @FXML private lateinit var refreshButton: Button
     @FXML private lateinit var mainPane: Pane
 
+    @FXML private lateinit var orangePolygon: Polygon
+    @FXML private lateinit var bluePolygon: Polygon
+    @FXML private lateinit var yellowPolygon: Polygon
+
     var refreshAction: (Project) -> Unit = { }
 
 
     override fun initialize(url: URL?, resource: ResourceBundle?) {
         mainPane.styleProperty().bind(Bindings.concat("-fx-font-size: ${scale}px;"))
+        scalePolygons(arrayListOf(orangePolygon, bluePolygon, yellowPolygon))
         refreshButton.addEventHandler(MouseEvent.MOUSE_CLICKED) {
             refreshAction(project)
         }
