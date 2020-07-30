277954e30469aa38d9ce13ee7dc716e7edc58d15
nbirillo <nbirillo@mail.ru>
2020-07-30 21:48:23 +0300

    Add student id

--- src/main/kotlin/org/jetbrains/research/ml/codetracker/server/TrackerQueryExecutor.kt
+++ src/main/kotlin/org/jetbrains/research/ml/codetracker/server/TrackerQueryExecutor.kt
@@ -2,14 +2,12 @@ package org.jetbrains.research.ml.codetracker.server
 
 import org.jetbrains.research.ml.codetracker.*
 import com.intellij.openapi.application.PathManager
-import io.reactivex.internal.operators.maybe.MaybeDoAfterSuccess
 import org.jetbrains.research.ml.codetracker.models.Extension
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
 import java.io.File
 import java.lang.IllegalStateException
 import java.net.URL
@@ -21,53 +19,69 @@ object TrackerQueryExecutor : QueryExecutor() {
     private const val ACTIVITY_TRACKER_FILE_FIELD = "activitytracker"
     private val activityTrackerPath = "${PathManager.getPluginsPath()}/activity-tracker/" + ACTIVITY_TRACKER_FILE
 
-    var activityTrackerKey: String? = null
+    var studentId: String? = null
 
     init {
-        initActivityTrackerInfo()
+        initStudentId()
     }
 
-    private fun initActivityTrackerInfo() {
-        val currentUrl = URL(baseUrl + "activity-tracker-item")
-        logger.info("${Plugin.PLUGIN_ID}: ...generating activity tracker key")
+    private fun initStudentId() {
+        val currentUrl = URL(baseUrl + "student")
+        logger.info("${Plugin.PLUGIN_ID}: ...generating student id")
         val requestBody = ByteArray(0).toRequestBody(null, 0, 0)
         val request = Request.Builder().url(currentUrl).post(requestBody).build()
-        activityTrackerKey = executeQuery(request)?.let { it.body?.string() }
+        studentId = executeQuery(request)?.let { it.body?.string() }
     }
 
-    private fun sendTrackerData(file: File,
-                                fileFieldName: String,
-                                baseUrlSuffix: String,
-                                method: String,
-                                toAddActivityTrackerKey: Boolean = false,
-                                afterSuccessfulResponse: () -> Unit = { }) {
+    private fun getRequestBodyForStudentQuery(
+        codeTrackerKey: String,
+        activityTrackerKey: String
+    ): MultipartBody.Builder {
+        val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
+        requestBody.addFormDataPart("diId", codeTrackerKey)
+        requestBody.addFormDataPart("atiId", activityTrackerKey)
+        return requestBody
+    }
+
+    private fun getRequestBodyForDataQuery(file: File, fileFieldName: String): MultipartBody.Builder {
         if (file.exists()) {
-            val currentUrl = baseUrl + baseUrlSuffix
             logger.info("${Plugin.PLUGIN_ID}: ...sending file ${file.name}")
             val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
             requestBody.addFormDataPart(fileFieldName, file.name, file.asRequestBody("text/csv".toMediaType()))
-            if (toAddActivityTrackerKey) {
-                activityTrackerKey?.let { requestBody.addFormDataPart("activityTrackerKey", it) }
-            }
-            val response =  executeQuery(Request.Builder().url(currentUrl).method(method, requestBody.build()).build())
-            if (response.isSuccessful()) {
-                afterSuccessfulResponse()
-            } else {
-                logger.info("${Plugin.PLUGIN_ID}: cannot send $fileFieldName data")
-                throw IllegalStateException("Unsuccessful server response")
-            }
+            return requestBody
         } else {
-            logger.info("${Plugin.PLUGIN_ID}: file ${file.name} for $fileFieldName doesn't exist")
+            throw IllegalStateException("File ${file.name} for $fileFieldName doesn't exist")
         }
     }
 
+    private fun executeTrackerQuery(
+        requestBody: MultipartBody.Builder,
+        baseUrlSuffix: String,
+        method: String = "POST"
+    ): String? {
+        val currentUrl = baseUrl + baseUrlSuffix
+        val response = executeQuery(Request.Builder().url(currentUrl).method(method, requestBody.build()).build())
+        if (response.isSuccessful()) {
+            return response?.let { it.body?.string() }
+        }
+        throw IllegalStateException("Unsuccessful server response")
+    }
+
     fun sendCodeTrackerData(file: File) {
-        val isKeyNull = activityTrackerKey == null
-        sendTrackerData(file, CODE_TRACKER_FILE_FIELD, "data-item", "POST", !isKeyNull) {
-            if (!isKeyNull) {
-                sendTrackerData(File(activityTrackerPath), ACTIVITY_TRACKER_FILE_FIELD,
-                    "activity-tracker-item/$activityTrackerKey", "PUT")
+        val codeTrackerKey = executeTrackerQuery(getRequestBodyForDataQuery(file, CODE_TRACKER_FILE_FIELD), "data-item")
+        codeTrackerKey?.let {
+            val activityTrackerKey = executeTrackerQuery(
+                getRequestBodyForDataQuery(File(activityTrackerPath), ACTIVITY_TRACKER_FILE_FIELD),
+                "activity-tracker-item"
+            )
+            activityTrackerKey?.let {
+                executeTrackerQuery(
+                    getRequestBodyForStudentQuery(codeTrackerKey, activityTrackerKey),
+                    "student/$studentId",
+                    "PUT"
+                )
             }
+
         }
     }
 }
